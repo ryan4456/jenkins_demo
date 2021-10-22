a2b6f05726aeef1ad27409a38b502f22f61b2407
Ryan Li <ryan.li.2021@outlook.com>
2021-10-22 19:08:54 +1300

    add dummy code to test SonarQube

---
+++ src/main/java/com/luulsj/www/jenkinsdemo/Main.java
@@ -0,0 +1,10 @@
+package com.luulsj.www.jenkinsdemo;
+
+public class Main {
+
+    public void calculateSalary(int month){
+        int i = 0;
+        int j = 1;
+        int sum = 100 / 0;
+    }
+}
